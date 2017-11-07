a1afd4a53e57890a4f70d1a29354bf279924a03d
Rodrigo Estebanez <rodhom@gmail.com>
2017-11-07 16:03:00 +0000

    UTIL::ExpandWords#perform implemented and spec'ed

--- lib/util/expand_words.rb
+++ lib/util/expand_words.rb
@@ -1,3 +1,5 @@
+require_relative '../english_language/irregular_verbs'
+
 module UTIL
   class ExpandWords
     attr_reader :file_path
@@ -5,6 +7,17 @@ module UTIL
     def initialize(file_path)
       File.file?(file_path) || raise(LoadError, "no such file to load -- #{file_path}")
       @file_path = file_path
+      @irregular_verbs_instance = ::ENGLISH_LANGUAGE::IrregularVerbs.new
+    end
+
+    def perform
+      File.open(file_path).each_line.inject([]) do |acc, line|
+        if result = @irregular_verbs_instance.expand(line.chomp)
+          acc << result
+        else
+          acc << line
+        end
+      end.join
     end
 
   end
--- spec/lib/util/expand_words_spec.rb
+++ spec/lib/util/expand_words_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 require_relative '../../../lib/util/expand_words'
 
+require 'tempfile'
+
 describe UTIL::ExpandWords do
   subject { described_class }
   let(:most_common_words_path){ File.expand_path('../../../word_lists/basic_english/200_most_common_words.txt',__dir__)}
@@ -12,6 +14,38 @@ describe UTIL::ExpandWords do
 
     it "raises an error if the given file path doesn't exist" do
       expect { described_class.new('invalid-path') }.to raise_error(LoadError)
+    end
+  end
+
+  describe "#perform" do
+    before do
+      @tmp_file = Tempfile.new('perform')
+      @tmp_file.write(
+"write
+hello
+build
+")
+      @tmp_file.close
+    end
+
+    let(:expand_words_instance) { described_class.new(@tmp_file.path) }
+
+    it "expands the words" do
+      expect(expand_words_instance.perform).to eq(
+"write (v)
+wrote (v past)
+writ (v past)
+written (v participle)
+writ (v participle)
+hello
+build (v)
+built (v past)
+built (v participle)
+")
+    end
+
+    after do
+      @tmp_file.unlink
     end
   end
 end
