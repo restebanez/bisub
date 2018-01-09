652c193a2481484b0b54aab342c4d9f540977e9a
Rodrigo Estebanez <rodhom@gmail.com>
2018-01-09 15:24:23 +0000

    scheleton

---
+++ lib/expand_words.rb
@@ -0,0 +1,3 @@
+# CAE and FCE Lists only contain the base verb form,
+# it should be expanded so our Rate class doesn't have to worry about different forms
+# Find a word marked with (v), ie:
---
+++ lib/rate.rb
@@ -0,0 +1,5 @@
+# This class rates the english text difficulty based on the oxford university press wordlist: CAE or FCE
+# 1. Remove, personal pronouns, prepositions
+
+class Rate
+end
